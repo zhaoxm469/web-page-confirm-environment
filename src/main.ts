9fae49d2b1063bd67a2e217928b14919ec60a22d
zhaoxingming <zhaoxingming@nucarf.com>
2021-08-03 15:28:45 +0800

    refactor: 重构代码

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: zhaoxingming
  * @Date: 2021-07-16 02:43:04
- * @LastEditTime: 2021-07-21 11:04:24
+ * @LastEditTime: 2021-08-03 15:25:59
  * @LastEditors: vscode
  * @Description: 用于判断当前网页所在的运行环境
  *
@@ -9,95 +9,127 @@
 
 interface IClientUa {
     // 跟IOS客户端 端协商 往前端页面塞入的UA标识
-    ios: string;
+    ios?: string;
     // 跟Android客户端 端协商 往前端页面塞入的UA标识
-    android: string;
+    android?: string;
+    // 自定义UA
+    ua?: string;
 }
 
 const clientUaDef: IClientUa = {
     ios: '',
-    android: ''
+    android: '',
+    ua: ''
 };
 
-const UA = window.navigator.userAgent.toLowerCase();
-
-export default class WebPageConfirmEnvironment {
-    clientUa: IClientUa = clientUaDef;
-    ua: string = UA;
-
-    constructor(clientUa?: IClientUa) {
-        this.clientUa = Object.assign(clientUa, clientUaDef);
-        this.init();
-    }
-
-    init() {
-        this.isInnerAdr = new RegExp(`${this.clientUa.android}`, 'i').test(
-            this.ua
-        );
-
-        this.isInnerIos = new RegExp(`${this.clientUa.ios}`, 'i').test(this.ua);
-
-        this.isInnerApp = this.isInnerAdr || this.isInnerIos;
-    }
-
-    printUa() {
-        return window.navigator.userAgent.toLowerCase();
-    }
-
-    isAndroid: boolean = (() => {
-        return /Android/i.test(this.ua);
-    })();
-
-    isIos: boolean = (() => {
-        return /iPhone|iPod|iPad/i.test(this.ua);
-    })();
-
-    isWechat: boolean = (() => {
-        return /MicroMessenger/i.test(this.ua);
-    })();
-
-    isWxWork: boolean = (() => {
-        return /MicroMessenger/i.test(this.ua) && /wxwork/i.test(this.ua);
-    })();
-
-    isWxmp: boolean = (() => {
-        return (
-            /miniProgram/i.test(this.ua) ||
-            window.__wxjs_environment === 'miniprogram'
-        );
-    })();
-
-    isDingding: boolean = (() => {
-        return /DingTalk/i.test(this.ua);
-    })();
-
+class WebPageConfirmEnvironment {
+    ua: string = '';
     /**
-     * @description: 是否为手机端
-     * @return { boolean }
+     * @description: 是否在安卓APP内
      */
-    isMobile: boolean = (() => {
-        return /AppleWebKit.*Mobile.*/gi.test(this.ua);
-    })();
-
-    isPc: boolean = (() => {
-        return !this.isMobile;
-    })();
-
+    isAndroid: boolean = false;
+    /**
+     * @description: 是否在IOS APP内
+     */
+    isIos: boolean = false;
+    /**
+     * @description: 是否在微信浏览器中
+     */
+    isWechat: boolean = false;
+    /**
+     * @description: 是否在企业微信中
+     */
+    isWxWork: boolean = false;
+    /**
+     * @description: 是否在微信小程序内
+     */
+    isWxmp: boolean = false;
+    /**
+     * @description: 是否在钉钉浏览器中
+     */
+    isDingding: boolean = false;
+    /**
+     * @description: 是否在飞书浏览器中
+     */
+    isFs: boolean = false;
+    /**
+     * @description: 是否在手机浏览器中
+     */
+    isMobile: boolean = false;
+    /**
+     * @description: 是否在PC端
+     */
+    isPc: boolean = false;
     /**
      * @description: 是否在自家安卓APP内
-     * @return {*}
      */
     isInnerAdr: boolean = false;
-
     /**
      * @description: 是否在自家IOS APP内
-     * @return {*}
      */
     isInnerIos: boolean = false;
 
     /**
      * @description: 是否在自家APP内
-     * @return { boolean }
      */
     isInnerApp: boolean = false;
+
+    constructor(private clientUa: IClientUa) {
+        this.clientUa = Object.assign(clientUa, clientUaDef);
+        this.editUa(clientUa.ua!);
+        this.init();
+    }
+
+    init() {
+        this.isAndroid = /android/i.test(this.ua);
+
+        this.isIos = /iphone|ipod|ipad/i.test(this.ua);
+
+        this.isWechat = /micromessenger/i.test(this.ua);
+
+        this.isWxWork =
+            /micromessenger/i.test(this.ua) && /wxwork/i.test(this.ua);
+
+        this.isWxmp =
+            /miniprogram/i.test(this.ua) ||
+            window.__wxjs_environment === 'miniprogram';
+
+        this.isDingding = /dingtalk/i.test(this.ua);
+
+        this.isFs = /lark/i.test(this.ua);
+
+        this.isMobile = /applewebkit.*mobile.*/gi.test(this.ua);
+
+        this.isPc = !this.isMobile;
+
+        this.isInnerAdr =
+            this.isAndroid &&
+            Boolean(this.clientUa.android) &&
+            new RegExp(`${this.clientUa.android}`, 'i').test(this.ua);
+
+        this.isInnerIos =
+            this.isIos &&
+            Boolean(this.clientUa.ios) &&
+            new RegExp(`${this.clientUa.ios}`, 'i').test(this.ua);
+
+        this.isInnerApp = this.isInnerAdr || this.isInnerIos;
+    }
+
+    /**
+     * @description: 打印UA
+     * @return string
+     */
+    printUa(): string {
+        return this.ua;
+    }
+
+    private editUa(ua: string): void {
+        const UA = window.navigator.userAgent.toLowerCase();
+        this.ua = ua ? ua.toLowerCase() : UA;
+    }
+}
+
+export default function (clientUa?: IClientUa) {
+    Object.assign(clientUaDef, clientUa);
+    return new WebPageConfirmEnvironment(clientUaDef);
 }
